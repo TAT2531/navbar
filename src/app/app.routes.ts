f2a97b4f699210c40914d5a864b09a5d93d7cde0
TAT2531 <prudhviraj.g@tatvasoftwareservices.com>
2025-07-09 20:34:42 +0530

    hr-dashboard_added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { EmployeeDashboardComponent } from './dashboards/employee-dashboard/employee-dashboard.component';
+import { HrDashboardComponent } from './dashboards/hr-dashboard/hr-dashboard.component';
 
 export const routes: Routes = [
     {
         path: 'dashboards/employee', 
         component: EmployeeDashboardComponent 
+    },
+    {
+        path: 'hr-dashboard',
+        component: HrDashboardComponent
     }
-
 ];
